c6ef1c638559e07445d27cf0ab9a2c2e4038fa3f
iaadamovich@gmail.com <i.a.adamovich@gmail.com>
2025-01-01 21:34:16 +0300

    improvements

--- software/devmem.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-setpci -s c1:00.0 COMMAND=0x102 # replace c1:00.0 with FPGA_DEVICE_ID in your OS
-echo 1 > /sys/bus/pci/devices/0000\:c1\:00.0/remove #:c1\00.0  ---> FPGA_DEVICE_ID in your OS
-echo 1 > /sys/bus/pci/devices/0000\:c0\:01.1/rescan # scan previous device
